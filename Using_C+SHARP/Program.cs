5459084367fc5b0dc0ee04e3c3583af55e343366
ByMaluf <brenno.maluf@gmail.com>
2022-11-27 13:33:11 -0300

    Segundo Commit

--- Using_C+SHARP/Program.cs
+++ Using_C+SHARP/Program.cs
@@ -12,11 +12,12 @@ namespace Using_C_SHARP
                             Está é a penultima linha
                             Esta é a última linha";
 
+            string item; //Criamos uma variável do tipo string com o nome de item 
+
             //Aqui utilizamos o using pois está instrução fornece uma abordagem melhor para destruição de arquivos
             //criamos a variável leitor do tipo var que recebe a Classe StringReader derivada da classe TextReader que fará a leitura do arquivo linhas.
             using (var leitor = new StringReader(linhas)) 
-            {
-                string item; //Criamos uma variável do tipo string com o nome de item 
+                            
                 do //Estrutura de repetição Do while (Faça enquanto), irá executar se o arquivo dentro de item for diferente de nulo.
                 {
                     item = leitor.ReadLine(); //Item recebe o leitor que é o tipo var(maneira implícita ou indireta de definir os dados),
@@ -24,7 +25,6 @@ namespace Using_C_SHARP
 
                     Console.WriteLine(item); //Se tudo estiver correto será printado na tela.
                 } while (item != null); //O que está dentro do do while só irá executar se o arquivo for dirente de nulo.
-            }
         }
     }
 }
